cde83cae8e21044a30e30e6c156597c5e3d676d7
tajwenzel <nwenzel@uw.edu>
2016-07-20 09:54:38 +0100

    Add files via upload
    Major functions and inital UK input for flu evidence synthesis package.

---
+++ FUNC_LL.R
@@ -0,0 +1,67 @@
+#Source file for fluEvidenceSynthesis for changing likelihood. Options for varying age group sizes, risk groups, (and susceptibility, and ascertainment through the starting parameters). Adapted from code by Baguelin 2013 and Edwin van Leevvan, by TajWenzel.
+
+#pars<-initial.parameters;
+#polymod<-polymod_uk
+#age.group.limits<-age.group.limits
+#age.group.sizes<-age.group.sizes
+#riskratios<-risk.ratios.null
+
+
+
+build.llikelihood<-function()
+  {
+llikelihood.f <- function(pars, agegrouplimits, agegroupsizes, riskratios,...)
+  {
+  proposed.contact.ids <<- current.contact.ids
+    if (runif(1,0,1) < 0.1) {
+      rs <<- round(runif(2,1,length(proposed.contact.ids)))
+      proposed.contact.ids[rs[1]] <<- rs[2]
+    }
+    # Resample contact ids. T
+    contacts<- contact.matrix(as.matrix(polymod[proposed.contact.ids,]),
+                              age_sizes[,1], agegrouplimits )
+    
+    
+    age.groups <- stratify_by_age(age_sizes[,1], agegrouplimits)
+    
+    # Population sizes in each age and risk group
+    popv <- stratify_by_risk(age.groups,riskratios) #agegroups*risk groups
+    
+    
+    epsilons <- c(pars[1], pars[1], pars[2], pars[2], pars[2],pars[3],pars[3]); #epsilon is         ascertainment
+    
+    initial.risk<-(rep(10^pars[9], length(riskratios)));
+    initial.infected <- stratify_by_risk(initial.risk, riskratios) 
+    odes <- infectionODEs(popv, initial.infected,
+                          vaccine_calendar,
+                    contacts,c(pars[6], pars[6], pars[6],pars[7], pars[7], pars[7], pars[8]),
+                          transmissibility = pars[5],
+                          infection_delays=c(0.8,1.8), interval=7 ) #interval is in days
+    
+    
+    # Ignore times row
+    relevant.range<-(nrow(riskratios)*length(age.groups))+1
+    converted.odes <- odes[,2:(relevant.range)];
+    dateaxis<-odes[,1]
+    
+    #Convert age groups and risk groups
+    #converted.odes[,1] <- rowSums(odes[,1])
+    #converted.odes[,2] <- rowSums(odes[,c(2,3)])
+    #converted.odes[,3] <- rowSums(odes[,c(4,5)])
+    #converted.odes[,4] <- rowSums(odes[,c(6)])
+    #converted.odes[,5] <- rowSums(odes[,c(7)])
+    #converted.odes <- converted.odes[,1:5]
+    
+    matplot(dateaxis,converted.odes[,1:length(converted.odes)], type='l')
+    legend('topleft',legend=1:length(converted.odes),col=1:length(converted.odes), pch=2)
+    # For each week and each group sum log likelihood
+    
+    ll<-log_likelihood_cases(
+      epsilons,pars[4], as.matrix(converted.odes),
+      agegroupsizes, ili$ili, ili$total.monitored,
+      confirmed.samples$positive, confirmed.samples$total.samples)
+    return(ll)
+  }
+  return(llikelihood.f)
+}
+
---
+++ FUNC_vstrategy.R
@@ -0,0 +1,33 @@
+#Source file for fluEvidenceSynthesis with access to vaccine coverage options. Adapted from code by Baguelin 2013 and Edwin van Leevvan, by TajWenzel.
+
+#load data in
+vstrategy<-function(riskratio)
+  {
+data<-data(coverage)
+cov <- coverage[,c("at.risk.under.65","X65")]/100.0
+
+  #efficacy
+  efficacy <- rep(c(0.7, 0.7, 0.7, 0.7, 0.7, 0.3, 0.3), nrow(riskratio))
+  dates <-coverage$Date
+
+          #set empty storage matrix on the dimension agegroups*time data=nrows, ncol=efficacy
+          calendar <- matrix(rep(0),nrow=length(dates),ncol = length(efficacy))
+
+
+############SETTING UP VACCINE PROGRAM, DEFINE WHICH AGE GROUPS ARE VACCINATED 
+# Set rate of vaccine uptake for different dates/age groups
+
+t=1:length(dates);
+calendar[t,c(1)] <- 0 #<6 months old
+calendar[t,c(2)] <-coverage[,c("Under.65")]/100.0 #2-4 year olds
+calendar[t,c(3)] <-coverage[,c("Under.65")]/100.0 #5-10, inclusive
+calendar[t,c(4)] <-coverage[,c("Under.65")]/1000.0 #11-15
+calendar[t,c(5)] <-0 #16-24
+calendar[t,c(6,7)] <-0 #adults 25+, 45+ and seniors
+
+        #compile vaccine schedule
+        vaccine_calendar <- as.vaccination.calendar(efficacy = efficacy, dates = dates, 
+                                            coverage = calendar)
+
+return(vaccine_calendar)
+}
