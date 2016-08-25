8d04239241ff14ad7fc2efbc599b4c8d8f90e6f8
tajwenzel <nwenzel@uw.edu>
2016-08-25 22:45:59 +0100

    further work on vaccination strategies

--- FUNC_cov_strategy.R
+++ FUNC_cov_strategy.R
@@ -7,52 +7,51 @@
 vstrategy<-function(riskratio,scenario,cov.eff,season)
   {
   
-cov <- cov.eff[[season]][,2:22]/100.0
- #USE COV INSTEAD OF COVERAGE SO DON'T HAVE TO DO THE /100 at every step
+   cov <- cov.eff[[season]][,2:22]/100.0
+    #USE COV INSTEAD OF COVERAGE SO DON'T HAVE TO DO THE /100 at every step
 
 ##################################################################################################
 #Efficacy
 ##################################################################################################
-  #efficacy.
-  eff.pull<-cov.eff[[season]][1,23:43] #pull first row of efficacies from matrix, matrix starts at 23 as 21 risk*age groups in original data +1 column for dates. This encompasses 2 risk*age groups (14 columns)
-  efficacy <- eff.pull
-  dates <-as.Date(cov.eff[[season]]$V1,origin="1970-01-01")
   
+    eff.pull<-cov.eff[[season]][1,23:43] 
+          #pull first row of efficacies from matrix, matrix starts at 23 as 21 risk*age groups in                  original data +1 column for dates. This encompasses 2 risk*age groups (14 columns)
+          
+    efficacy <- eff.pull
+    dates <-as.Date(cov.eff[[season]]$V1,origin="1970-01-01")
+     
 
           #set empty storage matrix on the dimension agegroups*time data=nrows, ncol=efficacy
           calendar <- matrix(rep(0),nrow=length(dates),ncol = length(efficacy))
 
-#scenario=1
+
 ############SETTING UP VACCINE PROGRAM, DEFINE WHICH AGE GROUPS ARE VACCINATED 
 # Set rate of vaccine uptake for different dates/age groups
           #additional analysis for amount of QALY's potentially lost due to mis-reporting from practices not responding
-          #(1,5,12,17,26,46,65)
           ####0) STATUS QUO is vaccination of elderly (65+) and high risk proportion (ages 0-85)
           if (scenario==1)
           {
             t=1:length(dates);
-            calendar[t,c(1)] <-cov[1]#<6 months old
-            calendar[t,c(2)] <-cov[2]
-            calendar[t,c(3)] <-cov[2]
-            calendar[t,c(4)] <-cov[2]
-            calendar[t,c(5)] <-cov[3]#17-25
-            calendar[t,c(6)] <-cov[3] #adults 26-45 
-            calendar[t,c(7)] <-cov[4]
-            calendar[t,c(8)] <-cov[4]#seniors 65+
-            calendar[t,c(9)] <- cov[5] #<6 months old
-            calendar[t,c(10)] <-cov[6] #1-4 year olds
-            calendar[t,c(11)] <-cov[7] #5-11, inclusive
-            calendar[t,c(12)] <-0 #11-16
-            calendar[t,c(13)] <-0 #17-25
-            calendar[t,c(14)] <-0 #adults 26-45 
-            calendar[t,c(15)] <-0 #adults 46-65 
+            calendar[t,c(1)] <-cov[[1]] #0-1
+            calendar[t,c(2)] <-cov[[2]] #1-4
+            calendar[t,c(3)] <-cov[[3]] #5-11
+            calendar[t,c(4)] <-cov[[3]] #12-14
+            calendar[t,c(5)] <-cov[[3]] #15-16
+            calendar[t,c(6)] <-cov[[4]] #17-24
+            calendar[t,c(7)] <-cov[[5]] #25-44
+            calendar[t,c(8)] <-cov[[6]] #45-64
+            calendar[t,c(9)] <-cov[[7]] #65-74
+            calendar[t,c(10)] <-cov[[7]] #75+
+            calendar[t,c(11)] <-0 
+            calendar[t,c(12)] <-0 
+            calendar[t,c(13)] <-0 
+            calendar[t,c(14)] <-0 
+            calendar[t,c(15)] <-0 
             calendar[t,c(16)] <-0
             calendar[t,c(17)] <-0
             calendar[t,c(18)] <-0
             calendar[t,c(19)] <-0
-            calendar[t,c(20)] <-0
-            calendar[t,c(21)] <-0
-            calendar[t,c(22)] <-cov[14] 
+            calendar[t,c(20)] <-cov[[14]] 
           }
             
 ####1) Preschool Vaccination only; ages 2-4     
